99dc1c4e71f94e96b9127051443085981672b3ed
Igor Bueno Franco <88065589+igBFranco@users.noreply.github.com>
2024-06-12 09:59:24 -0300

    Add Icon and SplashScreen

---
+++ app/src/main/java/br/pucpr/appdev20241/checklistfolha/controller/SplashActivity.kt
@@ -0,0 +1,22 @@
+package br.pucpr.appdev20241.checklistfolha.controller
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import br.pucpr.appdev20241.checklistfolha.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DISPLAY_LENGTH = 2000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH.toLong())
+    }
+}
