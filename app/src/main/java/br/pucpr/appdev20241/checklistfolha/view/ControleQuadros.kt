0553f3b3678810e466ebd42a0d0ac3a0f127b3e1
Igor Bueno Franco <88065589+igBFranco@users.noreply.github.com>
2024-08-27 20:24:27 -0300

    Adding Firebase Storage Integration

--- app/src/main/java/br/pucpr/appdev20241/checklistfolha/view/ControleQuadros.kt
+++ app/src/main/java/br/pucpr/appdev20241/checklistfolha/view/ControleQuadros.kt
@@ -22,10 +22,16 @@ import br.pucpr.appdev20241.checklistfolha.R
 import br.pucpr.appdev20241.checklistfolha.databinding.FragmentControleQuadrosBinding
 import br.pucpr.appdev20241.checklistfolha.model.DataStore
 import br.pucpr.appdev20241.checklistfolha.model.Quadro
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import java.util.Calendar
+import java.util.Date
+import java.util.UUID
 
 class ControleQuadros : Fragment() {
     private var _binding: FragmentControleQuadrosBinding? = null
@@ -71,11 +77,19 @@ class ControleQuadros : Fragment() {
 
         fab.setOnClickListener {
             val dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.adapter_input_quadros, null)
+            imageViewQuadro = dialogView.findViewById(R.id.imageViewQuadro)
+            val buttonSelectImage: Button = dialogView.findViewById(R.id.buttonSelectImage)
+
+            buttonSelectImage.setOnClickListener {
+                openImageSelector()
+            }
+
             val dialogBuilder = AlertDialog.Builder(requireContext())
                 .setView(dialogView)
                 .setTitle("Adicionar Entrega de Quadro de Frequência")
             val alertDialog = dialogBuilder.create()
             alertDialog.show()
+
             val editText = dialogView.findViewById<EditText>(R.id.editText)
             val datePicker = dialogView.findViewById<DatePicker>(R.id.datePicker)
 
@@ -88,7 +102,11 @@ class ControleQuadros : Fragment() {
                 calendar.set(year, month, day)
 
                 viewLifecycleOwner.lifecycleScope.launch {
-                    DataStore.addQuadroItem(Quadro(quadroLocal = text, dataEntrega = calendar.time))
+                    if (selectedImageUri != null) {
+                        saveQuadroWithImage(text, calendar.time)
+                    } else {
+                        saveQuadroWithoutImage(text, calendar.time)
+                    }
                     val updatedList = DataStore.getAllQuadros().toMutableList()
                     withContext(Dispatchers.Main) {
                         adapter.updateData(updatedList)
@@ -98,6 +116,39 @@ class ControleQuadros : Fragment() {
                 }
             }
         }
+    }
+
+    private fun openImageSelector() {
+        selectImageLauncher.launch("image/*")
+    }
+
+    private suspend fun saveQuadroWithImage(local: String, dataEntrega: Date) {
+        val uid = FirebaseAuth.getInstance().currentUser?.uid ?: return
+        val storageReference = FirebaseStorage.getInstance().reference
+        val imageRef = storageReference.child("images/$uid/${System.currentTimeMillis()}.jpg")
+
+        try {
+            val uploadTask = imageRef.putFile(selectedImageUri!!).await()
+            val downloadUrl = imageRef.downloadUrl.await()
+            val quadro = Quadro(
+                quadroLocal = local,
+                dataEntrega = dataEntrega,
+                imageUrl = downloadUrl.toString()
+            )
+            DataStore.addQuadroItem(quadro)
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), "Erro ao salvar o Quadro com imagem", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private suspend fun saveQuadroWithoutImage(local: String, dataEntrega: Date) {
+        val quadro = Quadro(
+            quadroLocal = local,
+            dataEntrega = dataEntrega
+        )
+        DataStore.addQuadroItem(quadro)
     }
 
     private suspend fun loadRecyclerView() {
@@ -157,35 +208,67 @@ class ControleQuadros : Fragment() {
                 setText(quadroItem?.quadroLocal)
             }
             val datePicker = dialogView.findViewById<DatePicker>(R.id.datePicker)
-            val calendar = Calendar.getInstance().apply { time = quadroItem?.dataEntrega }
-            datePicker.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+            val imageView = dialogView.findViewById<ImageView>(R.id.imageViewQuadroEdit)
+            val buttonSelectImage = dialogView.findViewById<Button>(R.id.buttonSelectImageEdit)
+
+            quadroItem?.imageUrl?.let { imageUrl ->
+                Glide.with(this@ControleQuadros)
+                    .load(imageUrl)
+                    .into(imageView)
+            }
+
+            buttonSelectImage.setOnClickListener {
+                selectImageLauncher.launch("image/*")
+            }
 
             val alertDialog = AlertDialog.Builder(requireContext()).run {
                 setView(dialogView)
                 setTitle("Editar Entrega de Quadro")
                 setPositiveButton("Salvar") { _, _ ->
                     val updatedText = editText.text.toString()
-                    quadroItem?.quadroLocal = updatedText
                     val year = datePicker.year
                     val month = datePicker.month
                     val day = datePicker.dayOfMonth
+                    val calendar = Calendar.getInstance()
                     calendar.set(year, month, day)
-                    quadroItem?.dataEntrega = calendar.time
-                    if (quadroItem != null) {
+                    val updatedDate = calendar.time
+
+                    if (selectedImageUri != null) {
                         viewLifecycleOwner.lifecycleScope.launch {
-                            DataStore.editQuadroItem(quadroItem)
-                            val updatedList = DataStore.getAllQuadros().toMutableList()
-                            adapter.updateData(updatedList)
-                            Toast.makeText(requireContext(), "Quadro editado com sucesso!", Toast.LENGTH_LONG).show()
+                            val imageUrl = uploadImageToFirebaseStorage(selectedImageUri!!)
+                            quadroItem?.imageUrl = imageUrl
+                            updateQuadro(quadroItem, updatedText, updatedDate)
                         }
+                    } else {
+                        updateQuadro(quadroItem, updatedText, updatedDate)
                     }
                 }
                 setNegativeButton("Cancelar", null)
             }.show()
             alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE)?.setTextColor(resources.getColor(R.color.red))
-
         }
     }
+
+    private fun updateQuadro(quadroItem: Quadro?, updatedText: String, updatedDate: Date) {
+        quadroItem?.apply {
+            quadroLocal = updatedText
+            dataEntrega = updatedDate
+            viewLifecycleOwner.lifecycleScope.launch {
+                DataStore.editQuadroItem(this@apply)
+                val updatedList = DataStore.getAllQuadros().toMutableList()
+                adapter.updateData(updatedList)
+                Toast.makeText(requireContext(), "Quadro editado com sucesso!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private suspend fun uploadImageToFirebaseStorage(imageUri: Uri): String {
+        val storageReference = FirebaseStorage.getInstance().reference
+        val imageRef = storageReference.child("quadros/${UUID.randomUUID()}.jpg")
+        imageRef.putFile(imageUri).await()
+        return imageRef.downloadUrl.await().toString()
+    }
+
 
     private fun configureRecycleViewEvent() {
         binding.rcvQuadros.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
